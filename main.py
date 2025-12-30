01c63014a5c569446293728277935227d54d6647
erybie222 <erybie222@gmail.com>
2025-12-30 10:18:28 +0100

    feat: find torso points, switch state

--- main.py
+++ main.py
@@ -1,18 +1,51 @@
 import pose_module
 import pose_module as pd
 import cv2
+import time
+
+###############################
+imgWidth, imgHeight = 1280, 720
+videosPath = "videos"
+###############################
+
 def main():
     # camera / video
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(f'{videosPath}/test_1.mp4')
 
-    detector = pd.PoseDetector()
+    detector = pd.PoseDetector(detectionCon=0.8, trackCon=0.8)
+    pTime = 0
 
     while True:
         success, img = cap.read()
+        img = cv2.resize(img, (imgWidth, imgHeight))
         img = detector.findPose(img)
+        lmList = detector.findPosition(img)
+
+        if len(lmList) != 0:
+            #rightArmAngle = detector.findAngle(img, 12 ,14, 16)
+            leftArmAngle = detector.findAngle(img, 11 ,13, 15)
+            state, reps = detector.checkState(img, "left")
+            if state:
+                cv2.putText(img, f'Phase: {state}', (20, 40), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+                cv2.putText(img, f'Reps: {reps}', (20, 90), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+
+            #leftElbowangle = detector.findAngle(img, 13, 11, 33)
+
+
+        cTime = time.time()
+        fps = 1/(cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, f'FPS: {str(int(fps))}' ,(1150, 30), cv2.FONT_HERSHEY_PLAIN , 2, (255,0, 255), 2)
+
         cv2.imshow("Bench press tracker", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+
+
+        if cv2.waitKey(50) & 0xFF == ord('q'):
             break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
--- pose_module.py
+++ pose_module.py
@@ -2,6 +2,14 @@ import mediapipe as mp
 import cv2
 import math
 
+States = {
+    0: "START",
+    1: "ECCENTRIC",
+    2: "BOTTOM",
+    3: "CONCENTRIC",
+    4: "FINISH"
+}
+
 class PoseDetector():
     def __init__(self, mode = False, smooth = True,
                  detectionCon=0.5, trackCon=0.5):
@@ -9,7 +17,9 @@ class PoseDetector():
         self.smooth = smooth
         self.detectionCon = detectionCon
         self.trackCon = trackCon
-
+        self.state = States[0]
+        self.repetitions= 0
+        self.barPath = []
         self.mpDraw = mp.solutions.drawing_utils
         self.mpPose = mp.solutions.pose
         self.pose = self.mpPose.Pose(static_image_mode = self.mode,
@@ -34,6 +44,9 @@ class PoseDetector():
                 self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+            self.findTorsoPoints(img)
+            self.lmList.append(self.leftTorso)
+            self.lmList.append(self.rightTorso)
         return self.lmList
 
     def findAngle(self, img, p1, p2, p3,draw=True ):
@@ -50,7 +63,7 @@ class PoseDetector():
         if angle > 180:
             angle = 360 - angle
 
-        print(angle)
+        #print(angle)
 
         if draw:
             cv2.line(img, (x1,y1), (x2,y2), (0, 0, 255), 3)
@@ -67,6 +80,56 @@ class PoseDetector():
             cv2.putText(img, str(int(angle)), (x2-50, y2+50), cv2.FONT_HERSHEY_PLAIN, 2, (0,0,255), 2)
 
         return angle
+
+    def findTorsoPoints(self, img , draw = True):
+        if len(self.lmList) != 0:
+            rightTorsoX = int((self.lmList[12][1] + self.lmList[24][1])/2)
+            rightTorsoY = int((self.lmList[12][2] + self.lmList[24][2])/2)
+
+            leftTorsoX = int((self.lmList[11][1] + self.lmList[23][1]) / 2)
+            leftTorsoY = int((self.lmList[11][2] + self.lmList[23][2]) / 2)
+
+            self.rightTorso = [34, rightTorsoX, rightTorsoY]
+            self.leftTorso = [33, leftTorsoX, leftTorsoY]
+            print(self.rightTorso, self.leftTorso)
+
+            if draw:
+                cv2.circle(img, (leftTorsoX, leftTorsoY), 5, (255, 0, 0), cv2.FILLED)
+                cv2.circle(img, (rightTorsoX, rightTorsoY), 5, (255, 0, 0), cv2.FILLED)
+
+    def checkState(self, img, side):
+        if side == "left":
+            p1, p2 , p3 = 11, 13, 15
+        elif side == "right":
+            p1, p2, p3 = 12, 14, 16
+        else:
+            return
+
+        armAngle = self.findAngle(img,p1, p2, p3)
+        wrist = self.lmList[15]
+
+        #Start
+        if self.state == States[0]:
+            if armAngle < 160:
+                self.state = States[1]
+        #Eccentric
+        elif self.state == States[1]:
+            self.barPath.append(wrist[1:])
+            if armAngle < 75:
+                self.state = States[2]
+        #Bottom
+        elif self.state == States[2]:
+            self.barPath.append(wrist[1:])
+            if armAngle > 85:
+                self.state = States[3]
+        #Concentric
+        elif self.state == States[3]:
+            self.barPath.append(wrist[1:])
+            if armAngle > 165:
+                self.repetitions += 1
+                self.state = States[0]
+        return self.state, self.repetitions
+
 
 
 
